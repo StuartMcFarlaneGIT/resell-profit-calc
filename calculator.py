e21aeeb6bea433d9ec21b7b14492bdd55225473f
StuartMcFarlaneGIT <stuartmc1101@outlook.com>
2026-08-31 22:05:30 +0100

    profit margin calculator added
    now calculates the margins by using the profit and sell price

--- calculator.py
+++ calculator.py
@@ -1,5 +1,12 @@
 platforms = ['ebay', 'vinted', 'facebook']
-promo = 0
+postage_fees = {
+    'ebay': 2.94,
+    'vinted': 0.00,
+    'facebook': 0.00
+}
+
+promo = 0.00
+postage = 0.00
 
 buy = float(input("How much did you pay? £"))
 sell = float(input("How much is it selling for? £"))
@@ -11,12 +18,20 @@ item = str(input("What item are you selling? "))
 if platform == 'ebay':
     promo = 0.03
 
+if platform in platforms:
+    postage = postage_fees[platform]
+
 def promotion_fees(sell, promo):
     promo_fee = float(sell*promo)
     return promo_fee
 
-def calculator(buy, sell, promo_fee):
-    return(sell - buy - promo_fee)
-profit = calculator(buy, sell, promotion_fees(sell, promo))
+def profit_calculator(buy, sell, promo_fee, postage):
+    return(sell - buy - promo_fee - postage)
 
-print(profit)
+profit = profit_calculator(buy, sell, promotion_fees(sell, promo), postage)
+
+def profit_percentage(sell, profit):
+    return((profit/sell)*100)
+margins = profit_percentage(sell, profit)
+
+print(f'Total profit = £{profit} \n Profit percentage = {margins:.2f}%')
