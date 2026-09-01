36c4cdcd7274994272eb24b0b97b711aa35f19f7
StuartMcFarlaneGIT <stuartmc1101@outlook.com>
2026-09-01 10:14:40 +0100

    error handling for the platform and categories

--- calculator.py
+++ calculator.py
@@ -25,43 +25,57 @@ def promotion_fees(sell, promo):
 def profit_calculator(buy, sell, promo_fee, postage):
     return(sell - buy - promo_fee - postage)
 
-def profit_percentage(sell, profit):
-    return((profit/sell)*100)
+def profit_percentage(buy, profit):
+    return((profit/buy)*100)
 
-promo = 0.00
-postage = 0.00
-buy = 0.00
-sell = 0.00
+while True:
+    promo = 0.00
+    postage = 0.00
+    buy = 0.00
+    sell = 0.00
 
-item = str(input("What item are you selling? "))
+    item = str(input("What item are you selling? "))
 
-category = input(f"Choose which platform you are selling on {categories}: ")
-if category not in categories:
-    print('enter one of the categories stated')
+    while True:
+        category = input(f"Choose which category you are selling on {categories}: ")
+        if category in categories:
+            break
+        if category not in categories:
+            print('enter one of the categories stated')
+            
+    while buy == 0.00:
+        try: 
+            buy = float(input(f"How much did {item} cost you? £"))
+        except ValueError:
+            print('Please only enter a number value')
 
-while buy == 0.00:
-    try: 
-        buy = float(input(f"How much did {item} cost you? £"))
-    except ValueError:
-        print('Please only enter a number value')
+    while sell == 0.00:
+        try:
+            sell = float(input("How much is it selling for? £"))
+        except ValueError:
+            print('Please only enter a number value')
 
-while sell == 0.00:
-    try:
-        sell = float(input("How much is it selling for? £"))
-    except ValueError:
-        print('Please only enter a number value')
+    while True:
+        platform = input(f"Choose which platform you are selling on {platforms}: ")
+        if platform in platforms:
+            break
+        if platform not in platforms:
+            print('enter one of the patforms stated')
 
-platform = input(f"Choose which platform you are selling on {platforms}: ")
-if platform not in platforms:
-    print('enter one of the patforms stated')
+    if platform == 'ebay':
+        promo = 0.03
 
-if platform == 'ebay':
-    promo = 0.03
+    if platform in platforms:
+        postage = postage_fees[platform]
 
-if platform in platforms:
-    postage = postage_fees[platform]
+    profit = profit_calculator(buy, sell, promotion_fees(sell, promo), postage)
+    margins = profit_percentage(buy, profit)
 
-profit = profit_calculator(buy, sell, promotion_fees(sell, promo), postage)
-margins = profit_percentage(sell, profit)
+    print(f'Total profit = £{profit:.2f} \n Profit percentage = {margins:.2f}%')
+    again = input("Do you have another item? (y/n): ").lower()
+    if again != 'y':
+        break
 
-print(f'Total profit = £{profit:.2f} \n Profit percentage = {margins:.2f}%')
+#print to csv file next
+#documentation, comments
+#try change input for categories and platform to choice to allow the user to select from the list instead of typing
